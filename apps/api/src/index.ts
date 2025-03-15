e2a6a25e8f50edbc8fdfa7d9b91c9794d806a643
Leonardo Ascione <112330494+rockfactory@users.noreply.github.com>
2025-03-15 02:47:05 +0100

    Web landing page

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import fs from 'fs/promises';
+import { engine } from 'express-handlebars';
 import { pb } from './core/pocketbase.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use('/static', express.static('public'));
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello, Civ world!' });
+  res.render('index', { title: 'Home', lang: 'en' });
 });
 
 app.post('/mods/import', async (req, res) => {});
