ed11c7b00877e68b902a2d931eb53297f88c45e0
Leonardo Ascione <112330494+rockfactory@users.noreply.github.com>
2025-03-30 19:47:55 +0200

    chore: Revert to `.po` files

--- apps/desktop/lingui.config.js
+++ apps/desktop/lingui.config.js
@@ -4,7 +4,7 @@ import { formatter } from '@lingui/format-json';
 export default defineConfig({
   sourceLocale: 'en_US',
   locales: ['it_IT', 'en_US', 'fr_FR', 'de_DE', 'es_ES', 'ko_KR'],
-  format: formatter({ style: 'lingui' }),
+  format: 'po',
   catalogs: [
     {
       path: '<rootDir>/src/locales/{locale}/messages',
